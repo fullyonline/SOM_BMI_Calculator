a56343350bb751912ebaaa6fe39a905e4b9958fc
MGBChrome <roman.inglin@gmail.com>
2022-12-20 21:22:19 +0100

    Added saving and loading of userlist

--- lib/config/UserSettings.dart
+++ lib/config/UserSettings.dart
@@ -46,11 +46,13 @@ class DropDownWidget extends State {
     Widget okButton = TextButton(
       child: const Text("OK"),
       onPressed: () {
-        setState(() {
-          spinnerItems.add(enteredUser);
-          dropdownValue = enteredUser;
-        });
-
+        if(!spinnerItems.contains(enteredUser)) {
+          setState(() {
+            spinnerItems.add(enteredUser);
+            dropdownValue = enteredUser;
+          });
+          UserPreferences.saveUserList(spinnerItems);
+        }
         UserPreferences.saveUser(enteredUser);
         Navigator.of(context).pop();
       },
@@ -86,69 +88,71 @@ class DropDownWidget extends State {
     }
   }
 
-  // Future saveUser(String username) async {
-  //   SharedPreferences prefs = await SharedPreferences.getInstance();
-  //   prefs.setString('selectedUser', username);
-  // }
-  //
-  // Future<String?> loadUser() async {
-  //   SharedPreferences prefs = await SharedPreferences.getInstance();
-  //   var loadedUser = prefs.getString('selectedUser');
-  //   return loadedUser;
-  // }
-
   Widget build(BuildContext context) {
+    Future<List<String>?> _usersFuture = UserPreferences.loadUserList();
+
     return Scaffold(
       body: Center(
-        child: Column(
-          children: <Widget>[
-            DropdownButton(
-              value: dropdownValue,
-              icon: Icon(Icons.arrow_drop_down),
-              iconSize: 24,
-              elevation: 16,
-              style: TextStyle(color: Colors.black, fontSize: 18),
-              underline: Container(
-                height: 2,
-                color: Colors.deepPurpleAccent,
-              ),
-              onChanged: (data) {
-                setState(() {
-                  dropdownValue = data!;
-                });
-              },
-              items: spinnerItems.map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ),
-            Text(
-              'Selected Item = ' + '$dropdownValue',
-              style: TextStyle(
-                fontSize: 22,
-                color: Colors.black
-              )
-            ),
-            ElevatedButton(
-              onPressed: () async => await onPressed(context),
-              child: const Text('Create user'),
-            ),
-            ElevatedButton(
-              onPressed: () async {
-                await showUser();
-              },
-              child: const Text('Show user'),
-            ),
-            Text(
-                'Selected user = ' + shownUser,
-                style: TextStyle(
-                    fontSize: 22,
-                    color: Colors.black
-                )
-            ),
-          ],
+        child: FutureBuilder(
+          future: _usersFuture,
+          builder: (BuildContext context, AsyncSnapshot<List<String>?> snapshot) {
+            if (snapshot.hasData && snapshot.data != null) {
+              spinnerItems = snapshot.data!;
+              return Column(
+                children: <Widget>[
+                  DropdownButton(
+                    value: dropdownValue,
+                    icon: Icon(Icons.arrow_drop_down),
+                    iconSize: 24,
+                    elevation: 16,
+                    style: TextStyle(color: Colors.black, fontSize: 18),
+                    underline: Container(
+                      height: 2,
+                      color: Colors.deepPurpleAccent,
+                    ),
+                    onChanged: (data) {
+                      setState(() {
+                        dropdownValue = data!;
+                      });
+                    },
+                    items: spinnerItems.map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                  Text(
+                      'Selected Item = ' + '$dropdownValue',
+                      style: TextStyle(
+                          fontSize: 22,
+                          color: Colors.black
+                      )
+                  ),
+                  ElevatedButton(
+                    onPressed: () async => await onPressed(context),
+                    child: const Text('Create user'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      await showUser();
+                    },
+                    child: const Text('Show user'),
+                  ),
+                  Text(
+                      'Selected user = ' + shownUser,
+                      style: TextStyle(
+                          fontSize: 22,
+                          color: Colors.black
+                      )
+                  ),
+                ],
+              );
+            }
+            else {
+              return const Text("No users created");
+            }
+          }
         )
       )
     );
--- lib/preferences/UserPreferences.dart
+++ lib/preferences/UserPreferences.dart
@@ -11,4 +11,15 @@ class UserPreferences {
     var loadedUser = prefs.getString('selectedUser');
     return loadedUser;
   }
+
+  static Future saveUserList(List<String> users) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setStringList('Users', users);
+  }
+
+  static Future<List<String>?> loadUserList() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    var loadedUsers = prefs.getStringList('Users');
+    return loadedUsers;
+  }
 }
