960cadf6dffee844265d18175e890738e057ed48
Romano Sabbatella <romano_sabbi_1993@hotmail.com>
2022-12-20 20:40:04 +0100

    added the logged in user to the result screen

--- lib/calculator/BMIResult.dart
+++ lib/calculator/BMIResult.dart
@@ -1,11 +1,13 @@
+import 'package:bmi_calculator/preferences/UserPreferences.dart';
 import 'package:flutter/material.dart';
 
 import '../services/BmiDto.dart';
 
 class BMIResult extends StatelessWidget {
-  const BMIResult({super.key, required this.bmiDto});
+  BMIResult({super.key, required this.bmiDto});
 
   final BmiDto bmiDto;
+  final Future<String?> _currentUser = UserPreferences.loadUser();
 
   // This widget is the root of your application.
   @override
@@ -18,7 +20,16 @@ class BMIResult extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center, // horizontale Ausrichtung innerhalb der Column
           mainAxisSize: MainAxisSize.min, // vertikale Ausrichtung
-          children: [Text("BMI result:"), Text(bmiDto.bmiForForm), Text(bmiDto.bmiCategory.text)],
+          children: [
+            FutureBuilder(future: _currentUser,builder: (BuildContext context, AsyncSnapshot<String?> snapshot) {
+              if (snapshot.hasData && snapshot.data != null) {
+                return Text("Logged in User: ${snapshot.data}");
+              }
+              else {
+                return const Text("No user logged in.");
+              }
+            }),
+            Text("BMI result:"), Text(bmiDto.bmiForForm), Text(bmiDto.bmiCategory.text)],
         )
       )
     );
