86e4743264c1869ee727eb1a916516ea4ca5af9d
MGBChrome <roman.inglin@gmail.com>
2023-01-29 19:47:52 +0100

    Cosmetic code formatting

--- lib/calculator/BMIResult.dart
+++ lib/calculator/BMIResult.dart
@@ -24,15 +24,20 @@ class BMIResult extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center, // horizontale Ausrichtung innerhalb der Column
           mainAxisSize: MainAxisSize.min, // vertikale Ausrichtung
           children: [
-            FutureBuilder(future: _currentUser,builder: (BuildContext context, AsyncSnapshot<String?> snapshot) {
-              if (snapshot.hasData && snapshot.data != null) {
-                return Text("${AppLocalizations.of(context)!.loggedInUser}: ${snapshot.data}");
+            FutureBuilder(
+                future: _currentUser,
+                builder: (BuildContext context, AsyncSnapshot<String?> snapshot) {
+                  if (snapshot.hasData && snapshot.data != null) {
+                    return Text("${AppLocalizations.of(context)!.loggedInUser}: ${snapshot.data}");
+                  }
+                  else {
+                    return Text(AppLocalizations.of(context)!.noUserLoggedIn);
+                  }
               }
-              else {
-                return Text(AppLocalizations.of(context)!.noUserLoggedIn);
-              }
-            }),
-            Text(AppLocalizations.of(context)!.bmiResult), Text(bmiDto.bmiForForm), Text(bmiDto.bmiCategory.text)],
+            ),
+            Text(AppLocalizations.of(context)!.bmiResult),
+            Text(bmiDto.bmiForForm),
+            Text(bmiDto.bmiCategory.text)],
         )
       )
     );
